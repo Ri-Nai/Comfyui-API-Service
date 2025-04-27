995c00cd6ae5107ffedcce299712d196b2067e73
Ri-Nai <383821630@qq.com>
2025-04-27 14:42:04 +0800

    在主程序中新增从命令行获取端口号的功能，允许用户通过参数指定端口，提升了应用的灵活性。

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
 import uvicorn
 from app import create_app
+import sys
 
 app = create_app()
 
 if __name__ == "__main__":
     # --port=8000
     port = 8000
+    args = sys.argv
+    if len(args) > 1:
+        port = int(args[1])
     uvicorn.run("run:app", host="0.0.0.0", port=port, reload=True)
 
